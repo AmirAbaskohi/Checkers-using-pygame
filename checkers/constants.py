df613f447c192f20fc23fa55adb1ca3535195dac
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-29 22:23:36 +0330

    Crown imaging added

--- checkers/constants.py
+++ checkers/constants.py
@@ -9,3 +9,5 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GREY = (128, 128, 128)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
--- checkers/pieces.py
+++ checkers/pieces.py
@@ -1,5 +1,5 @@
 import pygame
-from .constants import RED, WHITE, SQUARE_SIZE, GREY
+from .constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
 
 class Piece:
 
@@ -10,7 +10,7 @@ class Piece:
 		self.row = row
 		self.col = col
 		self.color = color
-		self.king = False
+		self.king = True
 		if self.color == RED:
 			self.direction = -1
 		else:
@@ -30,6 +30,8 @@ class Piece:
 		radius = SQUARE_SIZE//2 -self.PADDING
 		pygame.draw.circle(win, self.color, (self.x, self.y), radius + self.OUTLINE)
 		pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+		if self.king:
+			win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
 
 	def __repr__(self):
 		return str(self.color)
